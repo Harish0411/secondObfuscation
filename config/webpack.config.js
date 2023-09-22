2b2d45798fe448daa9f32d758281a4ae1410fde3
Hariharan <[200~>
2023-09-22 18:10:24 +0530

    third

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -246,7 +246,7 @@ module.exports = function (webpackEnv) {
       level: 'none',
     },
     optimization: {
-      minimize: isEnvProduction,
+      minimize: true,
       minimizer: [
         // This is only used in production mode
         new TerserPlugin({
@@ -262,6 +262,7 @@ module.exports = function (webpackEnv) {
             compress: {
               ecma: 5,
               warnings: false,
+              drop_console:true,
               // Disabled because of an issue with Uglify breaking seemingly valid code:
               // https://github.com/facebook/create-react-app/issues/2376
               // Pending further investigation:
